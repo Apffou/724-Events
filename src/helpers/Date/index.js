cbe2cab2dbf46b6e119f222c8888e6a3dfb57c2d
Anaïs Zafiriou-Lejeune <anais.ljn@outlook.fr>
2024-02-14 16:22:14 +0100

    Les EventCards affichent désormais le bon mois

--- src/helpers/Date/index.js
+++ src/helpers/Date/index.js
@@ -13,4 +13,4 @@ export const MONTHS = {
   12: "décembre",
 };
 
-export const getMonth = (date) => MONTHS[date.getMonth()];
+export const getMonth = (date) => MONTHS[date.getMonth() + 1]; // Ajout de +1 pour afficher le bon mois (tableau commence toujours à 0)
