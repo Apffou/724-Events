b2c415d7baf5a0313b7ddfe60cf2c6d4921f513e
Anaïs Zafiriou-Lejeune <anais.ljn@outlook.fr>
2024-02-15 10:53:24 +0100

    Resolution du probleme des liens dans le menu de navigation

--- src/containers/Menu/index.js
+++ src/containers/Menu/index.js
@@ -4,18 +4,20 @@ import Logo from "../../components/Logo";
 
 import "./style.scss";
 
+// modification pour rendre les liens cliquables en modifiant le hasg fragment
+
 const Menu = () => (
   <nav>
     <Logo />
     <ul>
       <li>
-        <a href="#nos-services">Nos services</a>
+        <a href="#nos-services" onClick={() => (window.document.location.hash = "#nos-services")}>Nos services</a>
       </li>
       <li>
-        <a href="#nos-realisations">Nos réalisations</a>
+        <a href="#nos-realisations" onClick={() => (window.document.location.hash = "#nos-realisations")}>Nos réalisations</a>
       </li>
       <li>
-        <a href="#notre-equipe">Notre équipe</a>
+        <a href="#notre-equipe" onClick={() => (window.document.location.hash = "#notre-equipe")}>Notre équipe</a>
       </li>
     </ul>
     <Button title="contact" onClick={() => (window.document.location.hash = "#contact")}>
