e5f6e9a34b40c50669aedf8db1cd0b972679f2e4
Anaïs Zafiriou-Lejeune <anais.ljn@outlook.fr>
2024-02-19 14:52:26 +0100

    Modification de code pour faire apparaitre le dernier evenement, code pour le mentor

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -15,7 +15,11 @@ import { useData } from "../../contexts/DataContext";
 // Ajout des ID pour correspondre aux liens du menu de navigation
 
 const Page = () => {
-  const { last } = useData()
+  const { data } = useData();
+  // Classement des évènements par date pour récupérer la dernière prestation
+  const last = data?.events.sort((evtA, evtB) =>
+    new Date(evtB.date) - new Date(evtA.date)
+  )[0];
   return <>
     <header>
       <Menu />
@@ -118,13 +122,16 @@ const Page = () => {
     <footer className="row">
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
-        <EventCard
-          imageSrc={last?.cover}
-          title={last?.title}
-          date={new Date(last?.date)}
-          small
-          label="boom"
-        />
+        {/*  L'opérateur && restitue le composant*/}
+        {last && (
+          <EventCard
+            imageSrc={last?.cover}
+            title={last?.title}
+            date={new Date(last?.date)}
+            small
+            label="boom"
+          />
+        )}
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
