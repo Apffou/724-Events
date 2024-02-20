a4b7096b9d4427a0387ad487b68c803fd78b2c87
Anaïs Zafiriou-Lejeune <anais.ljn@outlook.fr>
2024-02-20 17:57:56 +0100

    Modification du css pour résoudre une erreur visuel, Modification du timeOut, modification d'une condition pour effacer l'erreur console

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,6 +8,7 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
 
+
   // PLacement du focus sur l'élément indiqué grâce au trie via la méthode sort
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
     // Changement de l'opérateur logique pour trier de la plus récente à la plus ancienne
@@ -16,7 +17,8 @@ const Slider = () => {
   const nextCard = () => {
     setTimeout(
       // setIndex Ajoute 1 Si index est inférieur à la longueur de byDateDesc -1, sinon on rénitialise à zéro, cela créer une boucle.
-      () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
+      // Si byDateDesc existe, alors ça prends sa longueur sinon 0 (enlève l'erreur dans la console)
+      () => setIndex(index < (byDateDesc?.length || 0) - 1 ? index + 1 : 0),
       5000
     );
   };
@@ -28,9 +30,9 @@ const Slider = () => {
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
 
-        <>
+        <div key={event.title}>
           <div
-            key={event.title}
+
             className={`SlideCard SlideCard--${index === idx ? "display" : "hide"
               }`}
           >
@@ -46,10 +48,11 @@ const Slider = () => {
           </div>
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {/* Changement de idx par index (useState) pour suivre la bonne image */}
+              {/* Changement de idx par index (useState) pour suivre la bonne image + Changement de la valeur de key pour la rendre unique */}
               {byDateDesc.map((_, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={_.date}
+                  readOnly
                   type="radio"
                   name="radio-button"
                   checked={index === radioIdx}
@@ -57,7 +60,7 @@ const Slider = () => {
               ))}
             </div>
           </div>
-        </>
+        </div>
       ))}
     </div>
   );
